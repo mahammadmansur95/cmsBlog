b8b0c0001eaed897a608fc571af11b9b4e16806a
Mansur Mahammad - LFI <mmahammad@linkfields.com>
2023-03-01 18:18:48 +0530

    Added two forms

--- pages/testPage.js
+++ pages/testPage.js
@@ -1,8 +1,94 @@
-import React from 'react'
+import React from 'react';
+import { Button, Checkbox, Form, Input, Alert } from 'antd';
 
 const testPage = () => {
+
+  const [showSuccessComponent, setShowSuccessComponent] = useState(false);
+
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&');
+  };
+
+  const onFinish = (values) => {
+    if (values[`bot-field`] === undefined) {
+      delete values[`bot-field`]
+    }
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'mansur', ...values }),
+    })
+      .then(() => setShowSuccessComponent(true))
+      .catch((error) => alert(error));
+  };
+
   return (
-    <div>testPage</div>
+    <>
+      <div>testPage</div>
+      {
+        showSuccessComponent ? ( <Alert message="Its wonderful Mansur" type="success" />) : null
+      }
+      <form
+        name='mansur'
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        hidden
+      >
+        <input type="text" name="username" />
+        <input type="password" name="password" />
+      </form>
+      <Form
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        name="ddd"
+        method="POST"
+      >
+        <Form.Item
+          label="Don't fill this out"
+          className={`hidden`}
+          style={{ display: `none` }}
+          name="bot-field"
+        >
+          <Input type={`hidden`} name="form-name" value="ddd" />
+        </Form.Item>
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   )
 }
 
